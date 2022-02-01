0a19268526ee7c8bee531f2fe4b9390da1ad0e22
dmnth <kborodin1@gmail.com>
2022-02-02 01:13:19 +0200

    json write

--- src/concurrent_api_requests.py
+++ src/concurrent_api_requests.py
@@ -2,6 +2,7 @@
 import itertools
 import requests
 import threading
+import json
 from time import perf_counter
 from threading import Thread
 from gen_mmsi import panama_mids
@@ -9,18 +10,30 @@ from queue import Queue
 
 URL = 'https://www.vesselfinder.com/api/pub/click/'
 HEADERS = {'User-Agent': "Mozilla/5.0"}
+FILTER = {
+        'types': [
+            'unknown',
+            'unknown type',
+            ],
+        'year': [2003],
+        }
 
 jobs = Queue()
 found_vessels = []
+outfile = '/media/postgres-data/vessels.json'
 
 def make_requests(url, q):
     counter = 0
-    while not q.empty():
-         response = requests.get(f'{URL}{q.get()}000', headers=HEADERS)
-         found_vessels.append(response.content)
-         counter += 1
-         if counter == 34:
-             return 
+    with open(outfile, 'w') as file:
+        while not q.empty():
+             response = requests.get(f'{URL}{q.get()}000', headers=HEADERS)
+             if response:
+                 vessel = response.json()
+             if vessel['type'].lower() not in FILTER['types']: 
+                 found_vessels.append(vessel)
+                 print(vessel['type'], vessel['name'])
+        vessels_object = json.dumps(found_vessels, indent=4)
+        file.write(vessels_object)
 
 def create_jobs(mid_list):
     for mid in mid_list:
@@ -38,12 +51,25 @@ def get_shit_done():
     for i in range(3):
         worker.join()
 
+def write_json(vessel_data):
+    with open('panama_vessels.json', 'w') as outfile:
+        outfile.write(vessel_data)
+
+
+
 
 if __name__ == "__main__":
     create_jobs(panama_mids)
     start = perf_counter()
-    res = get_shit_done()
-    print(res)
+    try:
+        get_shit_done()
+    except:
+        print("writing stuff")
+        with open(outfile, 'w') as file:
+            vessels_object = json.dumps(found_vessels, indent=4)
+            file.write(vessels_object)
+        
     end = perf_counter()
+    print(found_vessels)
     print(f'Today i wasted {end-start:.2f} seconds on MMSI {len(found_vessels)}')
 
