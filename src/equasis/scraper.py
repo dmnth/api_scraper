cf505f0c7c142eec07dd2bc65936ea96bd162f99
thdmn <kborodin1@gmail.com>
2022-02-10 21:41:13 +0200

    added regex

--- src/equasis/scraper.py
+++ src/equasis/scraper.py
@@ -9,19 +9,45 @@ from requests_html import HTMLSession
 
 __PATH__ = os.path.abspath(os.path.dirname(__file__))
 
-session = HTMLSession()
-response = session.get('http://localhost:3000')
-management_details = response.html.find('#collapse3', first=True).find('td')
-for elem in management_details:
-    print(elem.text)
+def get_equasis_data(url):
+    
+    data = {}
+    
+    session = HTMLSession()
+    response = session.get('http://localhost:3000')
 
-classification_info = response.html.find('#collapse4')
-print(classification_info)
-for el in classification_info:
-    print(el.text)
+    #######Regex#########################o
 
+    # (?<=Registered owner)\n[A-Z]+\s[A-Z]+
+    owner_re = re.compile('(?<=Registered owner)([A-Z]+\s[A-Z]+)')
+    ism_manager_re = re.compile('(?<=Manager)[A-Z]+\s[A-Z]+')
+    commercial_manager_re = re.compile('(?<=Commercial manager)[A-Z]+\s[A-Z]+')
 
-# Todo: parse values with regex
+    #####################################
 
+    re_string = ''
+    management_details = response.html.find('#collapse3', first=True)
+    print(management_details.text)
+    for elem in management_details:
+        re_string += elem.text
 
+    data['owner'] = owner_re.search(re_string).group()
+    data['ism_manager'] = ism_manager_re.search(re_string).group()
+    data['commercial_manager'] = commercial_manager_re.search(re_string).group()
+
+    classification_info = response.html.find('#collapse4')
+    '''
+    for el in classification_info:
+        print(el.text)
+    '''
+
+    # Todo: parse values with regex
+    # Create a legit vessel class 
+
+    return data 
+
+if __name__ == "__main__":
+    url = 'http://localhost:3000'
+    result = get_equasis_data(url)
+    print(result)
 
